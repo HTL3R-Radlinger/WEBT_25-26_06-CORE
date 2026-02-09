cea8f94cd94ca4f864ec889e537f1cf219120416
AlexBa16 <alexbachinger56+GitHub@gmail.com>
2026-02-09 17:03:06 +0100

    add all route add id route add notFound page

--- src/Controller/MealplanController.php
+++ src/Controller/MealplanController.php
@@ -9,12 +9,31 @@ use App\Services\Seeder\MealSeeder;
 
 final class MealplanController extends AbstractController
 {
-    #[Route('/menu/current', name: 'app_mealplan')]
-    public function index(): Response
+    #[Route('/menu/current', name: 'current_mealplan', methods: ['GET'])]
+    public function current(): Response
     {
         $meal_plans = MealSeeder::generate();
         return $this->render('mealplan/index.html.twig', [
             'plan' => $meal_plans[0],
+        ]);
+    }
+
+    #[Route('/menu/all', name: 'all_mealplans', methods: ['GET'])]
+    public function all(): Response
+    {
+        $meal_plans = MealSeeder::generate();
+        return $this->render('meal_plans.html.twig', [
+            'meal_plans' => $meal_plans,
+        ]);
+    }
+
+    #[Route('/menu/{id}', name: 'mealplan_with_id', methods: ['GET'])]
+    public function withId(int $id): Response
+    {
+        $meal_plans = MealSeeder::generate();
+        if (!isset($meal_plans[$id])) return $this->redirectToRoute('notfound_page');
+        return $this->render('mealplan/index.html.twig', [
+            'plan' => $meal_plans[$id],
         ]);
     }
 }
