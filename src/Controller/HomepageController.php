7eb5cb749c48ad9abf2edc8dc495e25f3a0ab187
AlexBa16 <alexbachinger56+GitHub@gmail.com>
2026-02-15 21:43:01 +0100

    add comments

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -12,6 +12,22 @@ final class HomepageController extends AbstractController
     #[Route('/', name: 'app_homepage', methods: ['GET'])]
     public function index(): Response
     {
+//        return new Response(
+//            <<<EOF
+//                <html>
+//                    <body>
+//                        <p>Hello World!</p>
+//                    </body>
+//                </html>
+//            EOF
+//        );
         return $this->render('home.html.twig');
     }
 }
+
+// _GET: ?hello=...
+// public function index(Request $request): Response
+// {
+//     $request->query->get('hello')
+//     ...
+// }
