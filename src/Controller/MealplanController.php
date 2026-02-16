e043ed4c7284babe3a444463455408f25975a2ce
AlexBa16 <alexbachinger56+GitHub@gmail.com>
2026-02-16 11:13:12 +0100

    add comments and use dependency injection

--- src/Controller/MealplanController.php
+++ src/Controller/MealplanController.php
@@ -7,20 +7,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Services\Seeder\MealSeeder;
 
+#[Route('/menu')]
 final class MealplanController extends AbstractController
 {
     // Controller for the current Mealplan (id = 0)
-    #[Route('/menu/current', name: 'current_mealplan', methods: ['GET'])]
-    public function current(): Response
+    #[Route('/current', name: 'current_mealplan', methods: ['GET'])]
+    public function current(MealSeeder $mealSeeder): Response
     {
-        $meal_plans = MealSeeder::generate();
+        $meal_plans = $mealSeeder::generate();
         return $this->render('mealplan/index.html.twig', [
             'plan' => $meal_plans[0],
         ]);
     }
 
     // Controller for all the Mealplans
-    #[Route('/menu/all', name: 'all_mealplans', methods: ['GET'])]
+    #[Route('/all', name: 'all_mealplans', methods: ['GET'])]
     public function all(): Response
     {
         $meal_plans = MealSeeder::generate();
@@ -31,7 +32,7 @@ final class MealplanController extends AbstractController
 
     // Controller for a specific Mealplan (id is given as route path)
     #[Route(
-        '/menu/{id}',
+        '/{id}',
         name: 'mealplan_with_id',
         requirements: ['id' => '\d+'],
         methods: ['GET']
